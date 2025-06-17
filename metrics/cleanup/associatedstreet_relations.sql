3d0cef2ce94f29978b526b9de73ff982a1c3a8a0
Guillaume Rischard <stereo@faffy.openstreetmap.org>
2025-06-17 20:31:37 +0000

    associatedstreet are long gone

--- metrics/cleanup/associatedstreet_relations.sql
+++
@@ -1,5 +0,0 @@
--- Title: associatedStreet relations
--- Description: Relations of type associatedStreet
-SELECT id AS osm_id, 'relation' AS osm_type
-FROM planet_osm_rels
-WHERE tags->'type' = 'associatedStreet';
