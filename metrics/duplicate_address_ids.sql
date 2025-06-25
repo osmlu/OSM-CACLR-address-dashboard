06d65351090b9195f8faa3b4bcedbceef00a2611
Guillaume Rischard <stereo@faffy.openstreetmap.org>
2025-06-25 22:35:34 +0000

    fix title

--- metrics/duplicate_address_ids.sql
+++ metrics/duplicate_address_ids.sql
@@ -1,4 +1,4 @@
--- Title: Context: Addresses without housenumber not in OSM
+-- Title: Context: Addresses present more than once in CACLR
 -- Description: Context: Addresses in CACLR sharing the same id_caclr_bat value, e.g. when a parcel got split and both parts get the old address
 SELECT id_caclr_bat,
        numero,
