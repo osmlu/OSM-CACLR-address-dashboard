317f212c99c69def3c62633fbe04928052da936d
Guillaume Rischard <github@stereo.lu>
2025-06-25 18:13:01 -0400

    Update metrics queries

--- metrics/duplicate_address_ids.sql
+++ metrics/duplicate_address_ids.sql
@@ -1,8 +1,11 @@
--- Title: Duplicate CACLR IDs
+-- Title: Context: Addresses without housenumber not in OSM
 -- Description: Context: Addresses in CACLR sharing the same id_caclr_bat value, e.g. when a parcel got split and both parts get the old address
-SELECT count(*) FROM (
-    SELECT id_caclr_bat
-    FROM addresses
-    GROUP BY id_caclr_bat, numero, rue, localite
-    HAVING count(id_caclr_bat) > 1
-) AS foo;
+SELECT id_caclr_bat,
+       numero,
+       rue,
+       localite,
+       COUNT(*) AS occurrences
+FROM addresses
+GROUP BY id_caclr_bat, numero, rue, localite
+HAVING COUNT(id_caclr_bat) > 1
+ORDER BY occurrences DESC, id_caclr_bat;
--- metrics/invalid_street_postcode.sql
+++ metrics/invalid_street_postcode.sql
@@ -1,7 +1,7 @@
 -- Title: Invalid street/postcode
 -- Description: Street and postcode pairs that do not exist in CACLR
 -- include osm_potential_addresses.sql
-SELECT osm_id, osm_type, "addr:housenumber", "addr:street", "addr:postcode", "addr:city"
+SELECT osm_id, osm_type, "addr:housenumber", "addr:street", "addr:postcode", "addr:city", "ref:caclr", "note:caclr"
 FROM osm_potential_addresses
 WHERE ("addr:street", "addr:postcode") NOT IN (
     SELECT rue, code_postal::text FROM addresses
