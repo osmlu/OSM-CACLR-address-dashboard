63cb55c386e7394258b9e80bd7ffc6747f1a25a1
Guillaume Rischard <stereo@faffy.openstreetmap.org>
2025-06-25 22:30:48 +0000

    include note:caclr in common osm_potential_addresses

--- includes/osm_potential_addresses.sql
+++ includes/osm_potential_addresses.sql
@@ -1,7 +1,7 @@
 WITH osm_potential_addresses AS (
     SELECT osm_id, 'node' AS osm_type,
            "addr:housenumber", "addr:street", "addr:postcode",
-           "addr:city", "ref:caclr"
+           "addr:city", "ref:caclr", "note:caclr"
     FROM planet_osm_point
     WHERE "addr:housenumber" IS NOT NULL
       AND "addr:street" IS NOT NULL
@@ -10,7 +10,7 @@ WITH osm_potential_addresses AS (
     UNION
     SELECT osm_id, 'way' AS osm_type,
            "addr:housenumber", "addr:street", "addr:postcode",
-           "addr:city", "ref:caclr"
+           "addr:city", "ref:caclr", "note:caclr"
     FROM planet_osm_polygon
     WHERE "addr:housenumber" IS NOT NULL
       AND "addr:street" IS NOT NULL
