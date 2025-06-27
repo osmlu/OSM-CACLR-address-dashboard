4f8bf2792ce3675e3a3eed03042286207a6c6c71
Guillaume Rischard <stereo@faffy.openstreetmap.org>
2025-06-27 22:54:55 +0000

    Improve metric title and description

--- metrics/addresses_without_housenumber.sql
+++ metrics/addresses_without_housenumber.sql
@@ -1,3 +1,3 @@
--- Title: Addresses without housenumber
--- Description: Entries in the dataset that have no house number
+-- Title: Context: Addresses without housenumber
+-- Description: Entries in the CACLR dataset that have no house number
 select * from addresses where numero is null;
