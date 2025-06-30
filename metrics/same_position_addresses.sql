b9f4e784eb015c3c3fbdecd11abbbbf5adf70f77
Guillaume Rischard <stereo@faffy.openstreetmap.org>
2025-06-30 20:07:50 +0000

    Refined agglutinated caclr metric

--- metrics/same_position_addresses.sql
+++ metrics/same_position_addresses.sql
@@ -1,10 +1,24 @@
--- Title: Context: Agglutinated addresses
--- Description: Multiple addresses in CACLR sharing identical coordinates
-SELECT count(*) FROM (
-    SELECT
-lat_wgs84,
-lon_wgs84
-    FROM addresses
-    GROUP BY lat_wgs84, lon_wgs84
-    HAVING count(*) > 1
-) AS foo;
+-- Title: Context: addresses agglutinated in CACLR
+-- Description: Addresses go on the biggest building of the parcel. If there's more than one address on the parcel, it's a mess, unless they're aligned by hand.
+
+SELECT   rue,
+         Count(lat_wgs84)                        AS count,
+         string_agg(numero, ',' order BY numero) AS numero,
+         rue,
+         localite,
+         code_postal,
+         id_caclr_rue,
+         string_agg(id_caclr_bat :: text, ';' ORDER BY id_caclr_bat) AS id_caclr_bat,
+         lat_wgs84,
+         lon_wgs84,
+         commune
+FROM     addresses
+GROUP BY rue,
+         code_postal,
+         id_caclr_rue,
+         lat_wgs84,
+         lon_wgs84,
+         commune,
+         localite,
+         rue
+HAVING   count(lat_wgs84) > 1
